72cb7b623f7272bcd526227e8f7e3e9afbfa5354
gmy33 <M.Gao-2@student.tudelft.nl>
2022-02-14 23:34:32 +0100

    Update schedule.cc

--- schedule.cc
+++ schedule.cc
@@ -350,10 +350,10 @@ Thread * Scheduler::select_next_thread()
 		// Some threads are available
 
 		incSchelen();
-		// model_print("limitation for shcelen: %d - prevent live lock \n", schelen_limit);
-		// model_print("current length: %d \n", getSchelen());
-		// print_chg();
-		// model_print("find change priority == scheduler length: %d \n", find_chgidx(getSchelen()));
+		model_print("limitation for shcelen: %d - prevent live lock \n", schelen_limit);
+		model_print("current length: %d \n", getSchelen());
+		print_chg();
+		model_print("find change priority == scheduler length: %d \n", find_chgidx(getSchelen()));
 
 		if(getSchelen() <= schelen_limit){
 			int threadpct = find_highest(thread_list, avail_threads);
@@ -366,10 +366,10 @@ Thread * Scheduler::select_next_thread()
 			thread = execution->getFuzzer()->selectThread(thread_list, avail_threads);
 		}
 		
-		// model_print("Scheduler picks thread: %d\n", id_to_int(thread->get_id()));
-		// print_lowvec();
-		// print_highvec();
-		// model_print("\n\n");
+		model_print("Scheduler picks thread: %d\n", id_to_int(thread->get_id()));
+		print_lowvec();
+		print_highvec();
+		model_print("\n\n");
 		
 		//original: randomly select
 		//thread = execution->getFuzzer()->selectThread(thread_list, avail_threads);
