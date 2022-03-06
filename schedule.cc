0af3f9b7a16a36e73e8ec5704760a32cba48a56c
GMYMingyu <gaomingyuming@gmail.com>
2022-03-06 21:53:04 +0100

    Update schedule.cc

--- schedule.cc
+++ schedule.cc
@@ -138,8 +138,8 @@ int Scheduler::find_highest(int* availthreads, int availnum){
 
 	int findhigh = 0;
 	while(findhigh < highsize && !highvec_flag){
-		int currthread = highvec[findhigh];
-		for(int i = 0; i < availnum; i++){
+		int currthread = highvec[findhigh]; // current high-prio thread
+		for(int i = 0; i <= availnum; i++){ // if it is enabled
 			if(availthreads[i] == currthread){
 				highvec_flag = true; // highvec has thread available
 				resid = currthread;
