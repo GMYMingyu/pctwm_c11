f21bfa3549e4292b981f23ef87ab3de88642612c
GMYMingyu <gaomingyuming@gmail.com>
2022-04-06 01:22:55 +0200

    Update execution.cc

--- execution.cc
+++ execution.cc
@@ -469,8 +469,9 @@ bool ModelExecution::process_read(ModelAction *curr, SnapVector<ModelAction *> *
 				model_print("localvec has no variable. randomly select from rf_set. \n");
 				model_print("rf_set size is: %u. \n", rf_set->size());
 				index = fuzzer->selectWrite(curr, rf_set);
-				model_print("the read external flag: %u \n", rf->checkexternal());
+				
 				rf = (*rf_set)[index];
+				model_print("the read external flag of action read: %u : of write action %u \n", curr->checkexternal(), rf->checkexternal());
 				(*rf_set)[index] = rf_set->back();
 				rf_set->pop_back();
 			}
