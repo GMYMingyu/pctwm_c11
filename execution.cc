2b833e2c6120b70b0904c8a8e001f2fb963f0b92
GMYMingyu <gaomingyuming@gmail.com>
2022-04-05 13:45:46 +0200

    Update execution.cc

--- execution.cc
+++ execution.cc
@@ -1984,6 +1984,7 @@ Thread * ModelExecution::action_select_next_thread(const ModelAction *curr) cons
 	}
 		// weak memory model - return the second highest thread when the we meet a change point
 	if(curr->is_read() && curr->checkexternal_const()){
+		model_print("now hang on: select the second highest thread.");
 		scheduler->print_current_avail_threads();
 		return get_thread(int_to_id(scheduler->get_scecond_high_thread()));
 	}
