08c2a5a956bcb6cef8c7743a03a65207db4610f2
GMYMingyu <gaomingyuming@gmail.com>
2022-04-06 23:16:04 +0200

    Update execution.cc

--- execution.cc
+++ execution.cc
@@ -783,7 +783,7 @@ void ModelExecution::process_thread_action(ModelAction *curr)
 bool ModelExecution::initialize_curr_action(ModelAction **curr)
 {	
 	
-	model_print("the type of action is %u, external flag is %u \n", (*curr)->get_type(),(*curr)->checkexternal());
+	model_print("the type of action is %u, external flag is %u \n", action_type_t[(*curr)->get_type()],(*curr)->checkexternal());
 	if((*curr)->checkexternal()){
 		ASSERT((*curr)->is_read() == true);
 		
