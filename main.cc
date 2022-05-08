8e07c7c0aaaac19fc4d145803361eaf52493afe8
GMYMingyu <gaomingyuming@gmail.com>
2022-05-09 00:39:03 +0200

    Update main.cc

--- main.cc
+++ main.cc
@@ -25,10 +25,10 @@ void param_defaults(struct model_params *params)
 	params->checkthreshold = 500000;
 	params->removevisible = false;
 	params->nofork = false;
-	params->maxscheduler = 32;
+	params->maxscheduler = 38;
 	params->bugdepth = 12;
 	params->version = 1;
-	params->maxinstr = 32;
+	params->maxinstr = 38;
 	params->history = 2;
 }
 
