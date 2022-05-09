9d5110088b5c9c5efc5bfce2d02f36e2194f5c64
GMYMingyu <gaomingyuming@gmail.com>
2022-05-09 17:18:33 +0200

    Update main.cc

--- main.cc
+++ main.cc
@@ -29,7 +29,7 @@ void param_defaults(struct model_params *params)
 	params->bugdepth = 6;
 	params->version = 1;
 	params->maxread = 30;
-	params->seed = 2;
+	params->seed = 0;
 }
 
 static void print_usage(struct model_params *params)
