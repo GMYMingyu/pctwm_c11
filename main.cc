7af4fe8ffb4a051a2c466d87fa2b68fcfe600352
GMYMingyu <gaomingyuming@gmail.com>
2022-05-10 23:03:00 +0200

    Update main.cc

--- main.cc
+++ main.cc
@@ -26,7 +26,7 @@ void param_defaults(struct model_params *params)
 	params->removevisible = false;
 	params->nofork = false;
 	// params->maxscheduler = 16;
-	params->bugdepth = 5;
+	params->bugdepth = 6;
 	params->version = 1;
 	params->maxinstr = 24;
 	params->history = 5;
