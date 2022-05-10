d4a4d7b2352e09f99d3a8d030351ea05e9b2d186
GMYMingyu <gaomingyuming@gmail.com>
2022-05-10 16:28:22 +0200

    comment some print

--- schedule.cc
+++ schedule.cc
@@ -119,9 +119,9 @@ int Scheduler::getRandom(int range){
 	// srandom(seed);
 	// srandom(20);
 			
-	int tmp = random();
-	model_print("seed: %lu \n", tmp);
-	int res = tmp % range;
+	// int tmp = random();
+	// model_print("seed: %lu \n", tmp);
+	int res = random() % range;
 	res = res < 1 ? 1 : res;
 	return res;
 }
@@ -473,7 +473,7 @@ Thread * Scheduler::select_next_thread()
 
 		incSchelen();
 		// model_print("limitation for shcelen: %d - prevent live lock \n", schelen_limit);
-		model_print("current length: %d \n", getSchelen());
+		// model_print("current length: %d \n", getSchelen());
 		// print_avails(thread_list, avail_threads);
 		// print_chg();
 		// model_print("find change priority == scheduler length: %d \n", find_chgidx(getSchelen()));
